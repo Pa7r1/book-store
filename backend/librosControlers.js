150b5329620cbe17256e95112193cb14f60db5dd
Pa7r1 <valentincarpio32@gmail.com>
2024-11-01 10:39:11 -0300

    agrego validaciones para busqueda

--- backend/librosControlers.js
+++ backend/librosControlers.js
@@ -1,23 +1,36 @@
 import { db } from "./db.js";
 
-export const buscarLibro = async (req, res) => {
+export const validarBusqueda = (req, res, next) => {
   const q = req.query.q;
-  const filtros = [];
-  const parametros = [];
 
   if (!q) {
     return res.status(400).send({ mesaje: "no hay busqueda" });
   }
 
+  if (q == ">" || q == "<" || q == `"` || q == "'" || q == ";") {
+    return res.status(400).send({ mensaje: "mala peticion" });
+  }
+
+  if (q.length > 100) {
+    return res.status(400).send({ mensaje: "peticion demasiado larga" });
+  }
+  next();
+};
+
+export const buscarLibro = async (req, res) => {
+  const q = req.query.q;
+  const filtros = [];
+  const parametros = [];
+
   if (q != undefined) {
-    filtros.push(`nombre_libro LIKE "%${q}%"`);
-    parametros.push(q);
+    filtros.push(`nombre_libro LIKE ?`);
+    parametros.push(`%${q}%`);
   }
 
   let sql = "select * from libro";
 
   if (filtros.length > 0) {
-    sql += ` where ${filtros}`;
+    sql += ` where ${filtros.join(" and ")}`;
   }
 
   const [libroEncontrado] = await db.execute(sql, parametros);
