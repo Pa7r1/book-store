2db203e74fbf9ce17207e6506daf80ed9fcaa05f
Pa7r1 <valentincarpio32@gmail.com>
2024-11-01 10:53:15 -0300

    separo en archivos para hacerlo mas escalable

--- backend/libros.js
+++ backend/libros.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { buscarLibro, validarBusqueda } from "./librosControlers.js";
-
+import { buscarLibro } from "./librosControlers.js";
+import { validarBusqueda } from "./middleware.js";
 export const libroRouter = express.Router();
 
 libroRouter.get("/librosControlers", validarBusqueda, buscarLibro);
--- backend/librosControlers.js
+++ backend/librosControlers.js
@@ -1,22 +1,6 @@
 import { db } from "./db.js";
 
-export const validarBusqueda = (req, res, next) => {
-  const q = req.query.q;
-
-  if (!q) {
-    return res.status(400).send({ mesaje: "no hay busqueda" });
-  }
-
-  if (q == ">" || q == "<" || q == `"` || q == "'" || q == ";") {
-    return res.status(400).send({ mensaje: "mala peticion" });
-  }
-
-  if (q.length > 100) {
-    return res.status(400).send({ mensaje: "peticion demasiado larga" });
-  }
-  next();
-};
-
+//busqueda de libros
 export const buscarLibro = async (req, res) => {
   const q = req.query.q;
   const filtros = [];
