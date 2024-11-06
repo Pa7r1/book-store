1e0b830b7e7d197815e4eb9fadedd0ffca6748b7
Pa7r1 <valentincarpio32@gmail.com>
2024-11-06 17:42:36 -0300

    limpio app.js y dejo solo un route

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,18 @@
 import express from "express";
 import cors from "cors";
-import { conectarDB } from "./db.js";
-import { usuarioRouter } from "./usuarios.js";
-import { authRouter } from "./auth.js";
-import {
-  busquedaRouter,
-  categoriasRouter,
-  allRouter,
-} from "./librosControlers.js";
+import Router from "./routes/rutas.js";
 
-conectarDB();
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(cors());
 
+app.use("/api/v1", Router);
+
 app.get("/", (req, res) => {
   res.send("todo ok desde api book-store");
 });
-
-app.use("/librosControlers", busquedaRouter, categoriasRouter, allRouter);
-app.use("/usuarios", usuarioRouter);
-app.use("/auth", authRouter);
 
 app.listen(port, () => {
   console.log(`servidor levantado en el puerto: ${port}`);
